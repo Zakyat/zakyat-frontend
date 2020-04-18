6b890449e0b91a815b6ed05f748c3f3578bd5980
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-18 17:55:19 +0300

    Add ESLint plugin for Vue-i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,23 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:vue-i18n/recommended',
   ],
   // add your custom rules here
   rules: {
     "comma-dangle": ["error", "always-multiline"],
     "semi": ["error", "always"],
     "no-console": ["warn", { allow: ["warn", "error"]}],
-  }
+    "vue-i18n/no-raw-text": ["warn", {
+      "ignoreNodes": ["v-icon"],
+      "ignorePattern": "^[-#:()&]+$",
+      "ignoreText": ["©"],
+    }]
+  },
+  settings: {
+    'vue-i18n': {
+      localeDir: './locales/en.json',
+    },
+  },
 }
