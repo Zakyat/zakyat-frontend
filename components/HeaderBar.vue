74f69069dba3b395fd93e009934c89641fcd4735
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-18 16:07:51 +0300

    Add Header component

---
+++ components/HeaderBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-system-bar dark height="50">
+    <v-icon>mdi-heart</v-icon>
+    Собрано 6 234 234 ₽ для инвалидов
+    <v-spacer />
+    <v-btn text small>
+      <v-icon medium>
+        mdi-magnify
+      </v-icon>
+      Search
+    </v-btn>
+    <v-btn text small>
+      <v-icon medium>
+        mdi-vk
+      </v-icon>
+      Vkontakte
+    </v-btn>
+    <v-btn text small>
+      <v-icon medium>
+        mdi-instagram
+      </v-icon>
+      Instagram
+    </v-btn>
+    <v-btn text small>
+      <v-icon medium>
+        mdi-account-circle
+      </v-icon>
+      Login
+    </v-btn>
+  </v-system-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+
+});
+</script>
