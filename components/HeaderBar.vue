21e60eb691e4d4ac8b1aacfa9676287ab9c478e2
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-06-01 22:30:25 +0300

    Remove the button's default text-transform

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -3,25 +3,25 @@
     <img src="@/assets/images/heart.svg" class="ml-12 mr-3">
     {{ $t('collected', [6234234]) }}
     <v-spacer />
-    <v-btn text small>
+    <v-btn text small class="text-none">
       <v-icon medium>
         mdi-magnify
       </v-icon>
       {{ $t('search') }}
     </v-btn>
-    <v-btn text small>
+    <v-btn text small class="text-none">
       <v-icon medium>
         mdi-vk
       </v-icon>
       {{ $t('VK') }}
     </v-btn>
-    <v-btn text small>
+    <v-btn text small class="text-none">
       <v-icon medium>
         mdi-instagram
       </v-icon>
       {{ $t('instagram') }}
     </v-btn>
-    <v-btn text small>
+    <v-btn text small class="text-none">
       <v-icon medium>
         mdi-account-circle
       </v-icon>
