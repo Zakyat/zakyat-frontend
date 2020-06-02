3da16a824c0511812052951a35378f551d17e692
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-06-03 01:12:36 +0300

    Replace carousel with Swiper

--- components/home/SliderBar.vue
+++ components/home/SliderBar.vue
@@ -1,34 +1,70 @@
 <template>
-  <v-carousel
-    cycle
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item
+  <Swiper :options="swiperOptions">
+    <SwiperSlide
       v-for="(slide, i) in slides"
       :key="i"
     >
-      <v-row
-        class="fill-height"
-        align="center"
-        justify="center"
-      >
-        <img :src="slide.src">
-      </v-row>
-    </v-carousel-item>
-  </v-carousel>
+      <v-img :src="slide.src" />
+    </SwiperSlide>
+    <v-btn slot="button-prev" class="swiper-button-prev" fab>
+      <v-icon x-large>
+        mdi-chevron-left
+      </v-icon>
+    </v-btn>
+    <v-btn slot="button-next" class="swiper-button-next" fab>
+      <v-icon x-large>
+        mdi-chevron-right
+      </v-icon>
+    </v-btn>
+  </Swiper>
 </template>
+
 <script lang="ts">
 import Vue from 'vue';
 
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+import { SwiperOptions } from 'swiper';
+import 'swiper/css/swiper.css';
+
 export default Vue.extend({
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
   data () {
     return {
       slides: [
-        { src: require('@/assets/images/slideshow.png'), title: 'Пункт приема и раздачи одежды' },
-        { src: require('@/assets/images/slideshow.png'), title: 'Пункт приема и раздачи одежды' },
+        { src: require('@/assets/images/slideshow.png') },
+        { src: require('@/assets/images/slideshow.png') },
       ],
+      swiperOptions: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      } as SwiperOptions,
     };
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.swiper-container {
+  width: 88%;
+  min-height: 200px;
+  padding: 0px 5%;
+  .swiper-slide {
+    width: 75%;
+    padding: 20px 0;
+  }
+  .swiper-button-prev, .swiper-button-next {
+    font-size: 2em;
+    font-weight: bold;
+    &::after {
+      display: none;
+    }
+  }
+}
+</style>
