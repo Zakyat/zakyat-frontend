41790d1c9526e9ccaf5054f2af5269b949ca373c
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-06-20 00:30:44 +0300

    Make the code style more consistent

--- components/Zakat/QuestionList.vue
+++ components/Zakat/QuestionList.vue
@@ -3,13 +3,15 @@
     <v-expansion-panel
       v-for="({question, answer}) in $t('zakat.questions')"
       :key="question"
-      class="my-3"
-      style="border-radius: 10px"
+      class="my-2"
+      style="border-radius: 10px;"
     >
       <v-expansion-panel-header class="font-weight-black">
         {{ question }}
       </v-expansion-panel-header>
-      <v-expansion-panel-content>{{ answer }}</v-expansion-panel-content>
+      <v-expansion-panel-content>
+        {{ answer }}
+      </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
