ad1cc1ebb6defcd5eb1a8a3ebe99fca7353fbb42
stwer1998 <stwer1998@mail.ru>
2020-06-24 13:54:50 +0300

    add touter-link to logo, change size buttons

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,22 +1,46 @@
 <template>
   <v-app-bar flat color="white" height="100" style="padding: 0 5%;">
-    <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
-    <v-spacer />
+    <router-link to="/">
+      <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
+    </router-link>
     <v-btn
       v-for="(page, i) in pages"
       :key="i"
       :to="page.link"
       text
       nuxt
-      active-class="green--text"
-      class="font-weight-bold mx-n2"
+      small
+      class="font-weight-bold black--text"
     >
       {{ page.text }}
     </v-btn>
-    <v-btn rounded dark color="primary" class="mx-2">
+    <v-btn
+      to="/zakat"
+      text
+      nuxt
+      small
+      class="font-weight-bold green--text"
+    >
+      {{ $t('links.pay') }}
+    </v-btn>
+    <v-btn
+      rounded
+      dark
+      color="primary"
+      class="mx-2"
+      small
+      height="40px"
+    >
       {{ $t('give_help') }}
     </v-btn>
-    <v-btn rounded dark color="accent">
+    <v-btn
+      rounded
+      dark
+      color="accent"
+      class="mx-2"
+      small
+      height="40px"
+    >
       {{ $t('get_help') }}
     </v-btn>
   </v-app-bar>
@@ -33,7 +57,6 @@ export default Vue.extend({
         { text: this.$t('links.reports'), link: '/' },
         { text: this.$t('links.news'), link: '/' },
         { text: this.$t('links.about'), link: '/' },
-        { text: this.$t('links.pay'), link: '/zakat' },
       ],
     };
   },
