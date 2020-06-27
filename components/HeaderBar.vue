0305eacbb202ca8ad08d1fbcda5a9daa18243236
stwer1998 <stwer1998@mail.ru>
2020-06-27 19:30:32 +0300

    fix width content and content centered

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,50 +1,52 @@
 <template>
-  <v-system-bar dark height="50" style="padding: 0 5%;">
-    <img src="@/assets/images/heart.svg" class="ml-12 mr-3">
-    <span class="white--text">{{ $t('collected', [rubles(amount)]) }}</span>
-    <v-spacer />
-    <v-btn text small @click="isSearcherOpen = !isSearcherOpen">
-      <v-icon color="white" size="20">
-        mdi-magnify
-      </v-icon>
-      {{ $t('search.buttonName') }}
-    </v-btn>
-    <v-btn text small>
-      <v-icon color="white" size="20">
-        mdi-vk
-      </v-icon>
-      {{ $t('VK') }}
-    </v-btn>
-    <v-btn text small>
-      <v-icon color="white" size="20">
-        mdi-instagram
-      </v-icon>
-      {{ $t('instagram') }}
-    </v-btn>
-    <v-btn text small>
-      <v-icon color="white" size="20">
-        mdi-account-circle
-      </v-icon>
-      {{ $t('login') }}
-    </v-btn>
-    <v-flex lg1 md2 xs4>
-      <LanguageSelector class="icon" />
-    </v-flex>
-    <v-autocomplete
-      v-show="isSearcherOpen"
-      class="searcher mx-auto"
-      :items="items"
-      :placeholder="$t('search.placeholder')"
-      :no-data-text="$t('search.noDataText')"
-      append-icon=""
-      height="60"
-      light
-      solo
-      rounded
-      shaped
-      clearable
-    />
-  </v-system-bar>
+  <div style="background-color: black">
+    <v-system-bar dark height="50" class="headerstyle">
+      <img src="@/assets/images/heart.svg" class="ml-12 mr-3">
+      <span class="white--text">{{ $t('collected', [rubles(amount)]) }}</span>
+      <v-spacer />
+      <v-btn text small @click="isSearcherOpen = !isSearcherOpen">
+        <v-icon color="white" size="20">
+          mdi-magnify
+        </v-icon>
+        {{ $t('search.buttonName') }}
+      </v-btn>
+      <v-btn text small>
+        <v-icon color="white" size="20">
+          mdi-vk
+        </v-icon>
+        {{ $t('VK') }}
+      </v-btn>
+      <v-btn text small>
+        <v-icon color="white" size="20">
+          mdi-instagram
+        </v-icon>
+        {{ $t('instagram') }}
+      </v-btn>
+      <v-btn text small>
+        <v-icon color="white" size="20">
+          mdi-account-circle
+        </v-icon>
+        {{ $t('login') }}
+      </v-btn>
+      <v-flex lg1 md2 xs4>
+        <LanguageSelector class="icon" />
+      </v-flex>
+      <v-autocomplete
+        v-show="isSearcherOpen"
+        class="searcher mx-auto"
+        :items="items"
+        :placeholder="$t('search.placeholder')"
+        :no-data-text="$t('search.noDataText')"
+        append-icon=""
+        height="60"
+        light
+        solo
+        rounded
+        shaped
+        clearable
+      />
+    </v-system-bar>
+  </div>
 </template>
 
 <script lang="ts">
@@ -70,6 +72,12 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.headerstyle {
+  padding: 0 5%;
+  max-width: 1440px;
+  min-width: 1000px;
+  margin: 0 auto;
+}
 .searcher {
   position: absolute;
   left: 0;
