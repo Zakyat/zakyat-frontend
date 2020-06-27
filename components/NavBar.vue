4ffc8f4362c930eb56cb67326873987d6312e42b
stwer1998 <stwer1998@mail.ru>
2020-06-27 19:32:12 +0300

    change height, change text style, width fixed and content centered

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,49 +1,51 @@
 <template>
-  <v-app-bar flat color="white" height="100" style="padding: 0 5%;">
-    <router-link to="/">
-      <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
-    </router-link>
-    <v-btn
-      v-for="(page, i) in pages"
-      :key="i"
-      :to="page.link"
-      text
-      nuxt
-      small
-      class="font-weight-bold black--text"
-    >
-      {{ page.text }}
-    </v-btn>
-    <v-btn
-      to="/zakat"
-      text
-      nuxt
-      small
-      class="font-weight-bold green--text"
-    >
-      {{ $t('links.pay') }}
-    </v-btn>
-    <v-btn
-      rounded
-      dark
-      small
-      color="primary"
-      class="ml-6"
-      height="40px"
-    >
-      {{ $t('give_help') }}
-    </v-btn>
-    <v-btn
-      rounded
-      dark
-      small
-      color="accent"
-      class="mx-2"
-      height="40px"
-    >
-      {{ $t('get_help') }}
-    </v-btn>
-  </v-app-bar>
+  <div style="background-color: white;">
+    <v-app-bar flat color="white" height="120" class="navbarstyle">
+      <router-link to="/">
+        <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
+      </router-link>
+      <v-btn
+        v-for="(page, i) in pages"
+        :key="i"
+        :to="page.link"
+        text
+        nuxt
+        small
+        class="black--text"
+      >
+        {{ page.text }}
+      </v-btn>
+      <v-btn
+        to="/zakat"
+        text
+        nuxt
+        small
+        class="green--text"
+      >
+        {{ $t('links.pay') }}
+      </v-btn>
+      <v-btn
+        rounded
+        dark
+        small
+        color="primary"
+        class="ml-6"
+        height="40px"
+      >
+        {{ $t('give_help') }}
+      </v-btn>
+      <v-btn
+        rounded
+        dark
+        small
+        color="accent"
+        class="mx-3"
+        height="40px"
+      >
+        {{ $t('get_help') }}
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 
 <script lang="ts">
@@ -64,6 +66,12 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.navbarstyle {
+  padding: 0 5%;
+  max-width: 1440px;
+  min-width: 1000px;
+  margin: 0 auto;
+}
 .v-btn--active::before {
   opacity: 0;
 }
