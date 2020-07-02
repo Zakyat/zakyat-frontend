6f80745feba1db8b91b92f80c9a842dd27e59d08
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-03 00:43:43 +0300

    Remove unneeded classes and wrappers Take each property as a separate prop

--- components/news/NewsCard.vue
+++ components/news/NewsCard.vue
@@ -1,14 +1,12 @@
 <template>
   <v-card
     flat
-    class="mb-5 pl-5 pr-5 pt-3 pb-2"
-    style="border-radius: 10px;"
+    class="mb-5 px-5 pt-3 pb-2"
   >
     <v-row>
       <v-col
         cols="4"
         md="4"
-        class="card-img-col"
       >
         <v-img
           :src="require('@/assets/images/news/1.png')"
@@ -21,28 +19,22 @@
         cols="8"
         md="8"
       >
-        <v-row style="min-height: 155px;">
-          <v-col
-            class="pa-0"
-          >
-            <v-card-title class="news-card-title pt-0">
-              {{ news.title }}
-            </v-card-title>
-            <v-card-text class="pb-0">
-              {{ news.description }}
-            </v-card-text>
-          </v-col>
-        </v-row>
+        <v-card-title class="headline pa-0">
+          {{ title }}
+        </v-card-title>
+        <v-card-text class="pa-0">
+          {{ content }}
+        </v-card-text>
         <v-row align="end">
           <v-card-text
             class="pb-0"
             style="color: #9da3a6;"
           >
-            {{ localeDate() }}
+            {{ localeDate }}
             <v-icon class="px-1" style="color: inherit;">
               mdi-circle-small
             </v-icon>
-            {{ news.author }}
+            {{ author }}
           </v-card-text>
         </v-row>
       </v-col>
@@ -51,32 +43,38 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropType } from 'vue';
 
 export default Vue.extend({
-  name: 'SingleNews',
+  name: 'SingleNewsCard',
   props: {
-    news: {
-      type: Object,
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date as PropType<Date>,
+      required: true,
+    },
+    author: {
+      type: String,
       required: true,
     },
   },
-  data () {
-    return {
-      d: new Date(2020, 6, 2, 10),
-      options: { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' },
-    };
-  },
-  methods: {
-    localeDate () {
-      return this.d.toLocaleDateString(this.$i18n.locale, this.options);
+  computed: {
+    localeDate (): string {
+      return this.date.toLocaleDateString(this.$i18n.locale, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      });
     },
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.news-card-title {
-  font-size: 24px;
-}
-</style>
