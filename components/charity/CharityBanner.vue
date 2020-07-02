822e89a17d25a29e3bd948f4b1d637b7dd8b5fd4
Ural <ural2001@gmail.com>
2020-07-02 14:20:18 +0300

    Make charity banner and import charity banner img

--- components/charity/CharityBanner.vue
+++ components/charity/CharityBanner.vue
@@ -1,4 +1,33 @@
 <template>
+  <v-card
+    flat
+    color="#F5B820"
+    class="pa-0 ma-0 charity-banner"
+  >
+    <v-row>
+      <v-col
+        cols="12"
+        md="8"
+        align-self="center"
+      >
+        <v-card-title
+          class="charity-banner-text"
+        >
+          Вместе мы спасаем жизни детей и взрослых, помогаем им стать здоровыми и счастливыми!
+        </v-card-title>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        class="pa-md-0"
+      >
+        <v-img
+          width="300"
+          :src="require('@/assets/images/banners/3.png')"
+        />
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -10,4 +39,14 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+.charity-banner {
+  border-radius: 15px !important;
+}
+
+.charity-banner-text {
+  color: white;
+  font-weight: bold;
+  font-size: 26px;
+  line-height: 36px;
+}
 </style>
--- pages/charity.vue
+++ pages/charity.vue
@@ -1,11 +1,17 @@
 <template>
+  <v-container>
+    <CharityBanner />
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import CharityBanner from '@/components/charity/CharityBanner.vue';
 
 export default Vue.extend({
-
+  components: {
+    CharityBanner,
+  },
 });
 </script>
 
