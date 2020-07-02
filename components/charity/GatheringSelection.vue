0339a8e0223ac721c6ca9f3a8bdf7e9fa224887b
Ural <ural2001@gmail.com>
2020-07-02 15:09:48 +0300

    Add selector and checkbox into GatheringSelection component

--- components/charity/GatheringSelection.vue
+++ components/charity/GatheringSelection.vue
@@ -3,7 +3,34 @@
     justify="space-between"
     class="mt-12 px-3"
   >
-    
+    <v-col
+      cols="12"
+      md="4"
+      class="pa-0 ma-0 mr-auto"
+    >
+      <v-select
+        append-icon="mdi-chevron-down"
+        outlined
+        rounded
+        flat
+        solo
+        placeholder="Выберите сбор"
+        :items="items"
+        class="pa-0 ma-0"
+      >
+      </v-select>
+    </v-col>
+    <v-spacer/>
+    <v-col
+      cols="auto"
+      class="pa-0 md-0"
+    >
+      <v-checkbox
+        class="pa-0 md-0"
+        label="Анонимно"
+      >
+      </v-checkbox>
+    </v-col>
   </v-row>
 </template>
 
@@ -12,6 +39,11 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'GatheringSelection',
+  data () {
+    return {
+      items: ['Бурганова Альфия, сбор 309', 'Садыков Амирхан, сбор 400'],
+    };
+  },
 });
 </script>
 
