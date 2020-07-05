53fdafc31a6c7d0876c390f351fcd1dba995daee
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-06 02:09:57 +0300

    Remove unneeded wrapper and styles

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,52 +1,50 @@
 <template>
-  <div style="background-color: black">
-    <v-system-bar dark height="50" class="header_style">
-      <img src="@/assets/images/heart.svg" class="ml-4 mr-3">
-      <span class="white--text">{{ $t('collected', [rubles(amount)]) }}</span>
-      <v-spacer />
-      <v-btn text small @click="isSearcherOpen = !isSearcherOpen">
-        <v-icon color="white" size="20">
-          mdi-magnify
-        </v-icon>
-        {{ $t('search.buttonName') }}
-      </v-btn>
-      <v-btn text small>
-        <v-icon color="white" size="20">
-          mdi-vk
-        </v-icon>
-        {{ $t('VK') }}
-      </v-btn>
-      <v-btn text small>
-        <v-icon color="white" size="20">
-          mdi-instagram
-        </v-icon>
-        {{ $t('instagram') }}
-      </v-btn>
-      <v-btn text small>
-        <v-icon color="white" size="20">
-          mdi-account-circle
-        </v-icon>
-        {{ $t('login') }}
-      </v-btn>
-      <v-flex lg1 md2 xs4>
-        <LanguageSelector class="icon" />
-      </v-flex>
-      <v-autocomplete
-        v-show="isSearcherOpen"
-        class="searcher mx-auto"
-        :items="items"
-        :placeholder="$t('search.placeholder')"
-        :no-data-text="$t('search.noDataText')"
-        append-icon=""
-        height="60"
-        light
-        solo
-        rounded
-        shaped
-        clearable
-      />
-    </v-system-bar>
-  </div>
+  <v-system-bar dark height="50" style="padding: 0 5%;">
+    <img src="@/assets/images/heart.svg" class="ml-4 mr-3">
+    <span class="white--text">{{ $t('collected', [rubles(amount)]) }}</span>
+    <v-spacer />
+    <v-btn text small @click="isSearcherOpen = !isSearcherOpen">
+      <v-icon color="white" size="20">
+        mdi-magnify
+      </v-icon>
+      {{ $t('search.buttonName') }}
+    </v-btn>
+    <v-btn text small>
+      <v-icon color="white" size="20">
+        mdi-vk
+      </v-icon>
+      {{ $t('VK') }}
+    </v-btn>
+    <v-btn text small>
+      <v-icon color="white" size="20">
+        mdi-instagram
+      </v-icon>
+      {{ $t('instagram') }}
+    </v-btn>
+    <v-btn text small>
+      <v-icon color="white" size="20">
+        mdi-account-circle
+      </v-icon>
+      {{ $t('login') }}
+    </v-btn>
+    <v-flex lg1 md2 xs4>
+      <LanguageSelector class="icon" />
+    </v-flex>
+    <v-autocomplete
+      v-show="isSearcherOpen"
+      class="searcher mx-auto"
+      :items="items"
+      :placeholder="$t('search.placeholder')"
+      :no-data-text="$t('search.noDataText')"
+      append-icon=""
+      height="60"
+      light
+      solo
+      rounded
+      shaped
+      clearable
+    />
+  </v-system-bar>
 </template>
 
 <script lang="ts">
@@ -72,12 +70,6 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.header_style {
-  padding: 0 5%;
-  max-width: 1440px;
-  min-width: 1000px;
-  margin: 0 auto;
-}
 .searcher {
   position: absolute;
   left: 0;
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,52 +1,50 @@
 <template>
-  <div style="background-color: white;">
-    <v-app-bar flat color="white" height="120" class="navbar_style">
-      <router-link to="/">
-        <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
-      </router-link>
-      <v-spacer />
-      <v-btn
-        v-for="(page, i) in pages"
-        :key="i"
-        :to="page.link"
-        text
-        nuxt
-        small
-        class="black--text"
-      >
-        {{ page.text }}
-      </v-btn>
-      <v-btn
-        to="/zakat"
-        text
-        nuxt
-        small
-        class="green--text"
-      >
-        {{ $t('links.pay') }}
-      </v-btn>
-      <v-spacer />
-      <v-btn
-        rounded
-        dark
-        small
-        color="primary"
-        height="40px"
-      >
-        {{ $t('give_help') }}
-      </v-btn>
-      <v-spacer />
-      <v-btn
-        rounded
-        dark
-        small
-        color="accent"
-        height="40px"
-      >
-        {{ $t('get_help') }}
-      </v-btn>
-    </v-app-bar>
-  </div>
+  <v-app-bar flat color="white" height="120" style="padding: 0 5%;">
+    <router-link to="/">
+      <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
+    </router-link>
+    <v-spacer />
+    <v-btn
+      v-for="(page, i) in pages"
+      :key="i"
+      :to="page.link"
+      text
+      nuxt
+      small
+      class="black--text"
+    >
+      {{ page.text }}
+    </v-btn>
+    <v-btn
+      to="/zakat"
+      text
+      nuxt
+      small
+      class="green--text"
+    >
+      {{ $t('links.pay') }}
+    </v-btn>
+    <v-spacer />
+    <v-btn
+      rounded
+      dark
+      small
+      color="primary"
+      height="40px"
+    >
+      {{ $t('give_help') }}
+    </v-btn>
+    <v-spacer />
+    <v-btn
+      rounded
+      dark
+      small
+      color="accent"
+      height="40px"
+    >
+      {{ $t('get_help') }}
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script lang="ts">
@@ -67,12 +65,6 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.navbar_style {
-  padding: 0 5%;
-  max-width: 1440px;
-  min-width: 1000px;
-  margin: 0 auto;
-}
 .v-btn--active::before {
   opacity: 0;
 }
