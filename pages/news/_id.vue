04e31a0edaef4ef60004b3a53cecb2ee08b6a0ef
Ural <ural2001@gmail.com>
2020-07-09 09:35:26 +0300

    Make other news section

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row
+    >
       <v-col
         cols="12"
         md="9"
@@ -14,6 +15,7 @@
         <GreyCard />
       </v-col>
     </v-row>
+    <OtherNews class="mt-10 mx-0 mb-10" />
   </v-container>
 </template>
 
@@ -21,11 +23,13 @@
 import Vue from 'vue';
 import SpecificNewsCard from '~/components/news/aboutNews/SpecificNewsCard';
 import GreyCard from '~/components/news/aboutNews/GreyCard';
+import OtherNews from '~/components/news/aboutNews/OtherNews';
 
 export default Vue.extend({
   components: {
     SpecificNewsCard,
     GreyCard,
+    OtherNews,
   },
 });
 </script>
