5017c829e25af62a859a75cfc1865580670e016d
Ural <ural2001@gmail.com>
2020-07-12 12:19:13 +0300

    Add white-space: pre wrap style to specific news

--- components/aboutNews/SpecificNewsCard.vue
+++ components/aboutNews/SpecificNewsCard.vue
@@ -14,7 +14,7 @@
     >
       {{ specificNews.title }}
     </h1>
-    <p class="mt-3 mb-2">{{ specificNews.content }}</p>
+    <p class="mt-3 mb-2 news-content">{{ specificNews.content }}</p>
     <v-card-text
       class="pa-0 mb-5"
       style="color: #9da3a6;"
@@ -68,3 +68,9 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style scoped>
+.news-content {
+  white-space: pre-wrap;
+}
+</style>
