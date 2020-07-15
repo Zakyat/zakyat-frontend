37f90ae2b117cb24aee5e190365a587349b5aa87
stwer1998 <stwer1998@mail.ru>
2020-07-15 16:36:48 +0300

    AboutUsBar change video and text width

--- components/home/AboutUsBar.vue
+++ components/home/AboutUsBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container style="padding: 3% 6%; max-width: 100%;">
+  <v-container class="about-container">
     <v-row no-gutters>
       <v-col cols="12">
         <h1 class="display-1 font-weight-bold">
@@ -18,10 +18,26 @@
         </div>
       </v-col>
       <v-col cols="12" md="6" class="text-right">
-        <video height="340px" controls style="border-radius: 10px;">
-          <source :src="require('@/assets/videos/aboutUs.mp4')" type="video/mp4">
-        </video>
+        <iframe
+          width="97%"
+          height="370"
+          src="https://www.youtube.com/embed/PNwRFQDfoD8"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<style lang="scss" scoped>
+.about-container {
+  padding: 1% 6%;
+  max-width: 100%;
+
+  p {
+    width: 97%;
+  }
+}
+</style>
