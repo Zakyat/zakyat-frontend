53d9d5bb911afb665e5ba674d531642a0f9d6ceb
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-15 18:01:05 +0300

    Use VCard's nuxt link prop instead of n-link wrapper

--- components/news/NewsCard.vue
+++ components/news/NewsCard.vue
@@ -1,50 +1,47 @@
 <template>
-  <nuxt-link
+  <v-card
+    flat
+    nuxt
     :to="`news/${id}`"
-    style="text-decoration: none;"
+    class="mb-5 px-5 pt-3 pb-2"
   >
-    <v-card
-      flat
-      class="mb-5 px-5 pt-3 pb-2"
-    >
-      <v-row>
-        <v-col
-          cols="4"
-          md="4"
-        >
-          <v-img
-            :src="require('@/assets/images/news/1.png')"
-            width="300px"
-            style="border-radius: 10px;"
-          />
-        </v-col>
+    <v-row>
+      <v-col
+        cols="4"
+        md="4"
+      >
+        <v-img
+          :src="require('@/assets/images/news/1.png')"
+          width="300px"
+          style="border-radius: 10px;"
+        />
+      </v-col>
 
-        <v-col
-          cols="8"
-          md="8"
-        >
-          <v-card-title class="headline pa-0">
-            {{ title }}
-          </v-card-title>
-          <v-card-text class="pa-0">
-            {{ content }}
+      <v-col
+        cols="8"
+        md="8"
+      >
+        <v-card-title class="headline pa-0">
+          {{ title }}
+        </v-card-title>
+        <v-card-text class="pa-0">
+          {{ content }}
+        </v-card-text>
+        <v-row align="end">
+          <v-card-text
+            class="pb-0"
+            style="color: #9da3a6;"
+          >
+            {{ localeDate }}
+            <v-icon class="px-1" style="color: inherit;">
+              mdi-circle-small
+            </v-icon>
+            {{ author }}
           </v-card-text>
-          <v-row align="end">
-            <v-card-text
-              class="pb-0"
-              style="color: #9da3a6;"
-            >
-              {{ localeDate }}
-              <v-icon class="px-1" style="color: inherit;">
-                mdi-circle-small
-              </v-icon>
-              {{ author }}
-            </v-card-text>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-card>
-  </nuxt-link>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script lang="ts">
