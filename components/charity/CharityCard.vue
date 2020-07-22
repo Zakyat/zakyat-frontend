2f989ae793ad35bac270a79ec0cddb99de6aa94d
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-22 06:40:45 +0300

    Destructure object into individual props

--- components/charity/CharityCard.vue
+++ components/charity/CharityCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-row>
+  <v-row no-gutters>
     <v-col
       cols="12"
       md="auto"
-      class="py-0 pl-0"
+      class="pr-3"
     >
       <v-img
         width="250"
@@ -12,32 +12,27 @@
         :src="require('@/assets/images/slideshow1.jpg')"
       />
     </v-col>
-    <v-col
-      class="pa-0"
-    >
-      <v-row>
-        <v-col
-          class="py-0"
-        >
-          <h2>{{ gathering.name }}, {{ gathering.age }} лет</h2>
-          <p>{{ gathering.problem }}</p>
+    <v-col>
+      <v-row no-gutters>
+        <v-col>
+          <h2>{{ name }}, {{ age }} лет</h2>
+          <p>{{ problem }}</p>
         </v-col>
         <v-col
-          class="py-0"
           cols="auto"
         >
           <v-progress-circular
             color="primary"
-            :value="gathering.collectedPercent"
+            :value="collectedPercent"
             size="70"
             rotate="-90"
           >
-            <b class="percent-text">{{ gathering.collectedPercent }}%</b>
+            <b class="percent-text">{{ collectedPercent }}%</b>
           </v-progress-circular>
         </v-col>
       </v-row>
       <p class="description mt-3">
-        {{ gathering.text }}
+        {{ description }}
       </p>
       <v-row justify="space-between" class="px-3 mt-8">
         <v-col
@@ -48,30 +43,33 @@
           <v-row
             justify="space-between"
             class="px-3"
+            no-gutters
           >
-            <div>
+            <v-col>
               <v-card-text class="pa-0 ma-0 progress-text">
                 {{ $t('charity.gathering.charityCard.collected') }}
               </v-card-text>
-              <b>{{ gathering.collected | rubles }}</b>
-            </div>
-            <div>
+              <b>{{ collected | rubles }}</b>
+            </v-col>
+            <v-spacer />
+            <v-col>
               <v-card-text class="pa-0 ma-0 progress-text">
                 {{ $t('charity.gathering.charityCard.left') }}
               </v-card-text>
-              <b>{{ gathering.left | rubles }}</b>
-            </div>
-            <div>
+              <b>{{ remaining | rubles }}</b>
+            </v-col>
+            <v-spacer />
+            <v-col class="text-right">
               <v-card-text class="pa-0 ma-0 progress-text">
                 {{ $t('charity.gathering.charityCard.total') }}
               </v-card-text>
-              <b>{{ gathering.total | rubles }}</b>
-            </div>
+              <b>{{ required | rubles }}</b>
+            </v-col>
           </v-row>
           <v-progress-linear
             class="pa-0"
             color="primary"
-            :value="gathering.collectedPercent"
+            :value="collectedPercent"
             height="8"
             rounded
             background-color="#DADADA"
@@ -83,9 +81,9 @@
           align-self="end"
           class="pa-0 ma-0"
         >
-          <n-link style="text-decoration: none;" to="/">
+          <nuxt-link style="text-decoration: none;" to="/">
             {{ $t('charity.gathering.charityCard.seeGatherings') }}
-          </n-link>
+          </nuxt-link>
         </v-col>
       </v-row>
     </v-col>
@@ -98,9 +96,41 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'CharityCard',
   props: {
-    gathering: {
-      type: Object,
+    id: {
+      type: Number,
       required: true,
+    },
+    collected: {
+      type: Number,
+      required: true,
+    },
+    required: {
+      type: Number,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    problem: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    remaining (): number {
+      return this.required - this.collected;
+    },
+    collectedPercent (): number {
+      return Math.floor(100 * this.collected / this.required);
     },
   },
 });
