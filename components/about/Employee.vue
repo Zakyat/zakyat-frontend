3d998f79b04613cd741aee38bd5756d9f1aec365
Ural <ural2001@gmail.com>
2020-07-25 08:48:41 +0300

    Move static text to locales and move empoyee info to parent component

--- components/about/Employee.vue
+++ components/about/Employee.vue
@@ -1,39 +1,28 @@
 <template>
-  <v-row>
-    <v-col
-      v-for="i in 5"
-      :key="i"
-      cols="12"
-      md="6"
-    >
-      <v-card
-        flat
-      >
-        <v-img
-          rounded
-          class="main-img"
-          :src="require('@/assets/images/about/102113 1.png')"
-        />
-        <v-card-title class="pa-0 mt-3" style="font-size: 26px;">
-          Марданов Булат Раифович
-        </v-card-title>
-        <v-card-subtitle class="pa-0 mt-1">
-          Генеральный директор БФ «Закят»
-        </v-card-subtitle>
-        <v-card-text class="pa-0 black--text">
-          Функции:
-        </v-card-text>
-        <v-card-text class="pa-0 black--text" style="width: 80%;">
-          1. Распоряжание средствами фонда.
-          2. Формирование задач, поставленных перед фондом и осуществление организации их решений.
-          3. Представление интересов фонда при взаимодействии с юридическими и физическими лицами.
-        </v-card-text>
-      </v-card>
-      <v-card-text class="pa-0 mt-3 black--text">
-        <v-icon>mdi-phone</v-icon> + 7 (843) 225-33-22
-      </v-card-text>
-    </v-col>
-  </v-row>
+  <v-card
+    flat
+  >
+    <v-img
+      rounded
+      class="main-img"
+      :src="img"
+    />
+    <v-card-title class="pa-0 mt-3" style="font-size: 26px;">
+      {{ name }}
+    </v-card-title>
+    <v-card-subtitle class="pa-0 mt-1">
+      {{ position }}
+    </v-card-subtitle>
+    <v-card-text class="pa-0 black--text">
+      Функции:
+    </v-card-text>
+    <v-card-text class="pa-0 black--text" style="width: 80%;">
+      {{ duties }}
+    </v-card-text>
+    <v-card-text class="pa-0 mt-3 black--text">
+      <v-icon>mdi-phone</v-icon> {{ phone }}
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -41,6 +30,28 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Employee',
+  props: {
+    img: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    duties: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
 });
 </script>
 
--- components/about/Employees.vue
+++ components/about/Employees.vue
@@ -1,7 +1,22 @@
 <template>
   <v-container class="pa-0">
-    <h2>Сотрудники фонда</h2>
-    <Employee />
+    <h2>{{ $t('about.employees') }}</h2>
+    <v-row>
+      <v-col
+        v-for="employee in employees"
+        :key="employee.id"
+        cols="12"
+        md="6"
+      >
+        <Employee
+          :img="employee.img"
+          :name="employee.name"
+          :position="employee.position"
+          :duties="employee.duties"
+          :phone="employee.phone"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -13,6 +28,12 @@ export default Vue.extend({
   name: 'Employees',
   components: {
     Employee,
+  },
+  props: {
+    employees: {
+      type: Array,
+      required: true,
+    },
   },
 });
 </script>
