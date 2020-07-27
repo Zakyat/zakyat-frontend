519ac507f74b48c96918e7f61f1802d641ad5b7c
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-27 21:39:23 +0300

    Require component names to match file names

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'semi': ['error', 'always'],
     'no-console': ['warn', { allow: ['warn', 'error'] }],
     'quote-props': ['error', 'consistent-as-needed'],
+    'vue/match-component-file-name': ['error', { extensions: ['vue'] }],
     /* 'vue-i18n/no-raw-text': ['warn', {
       ignoreNodes: ['v-icon'],
       ignorePattern: '^([\\W\\d\\s]+|\\w+@\\w+\\.\\w+)$',
