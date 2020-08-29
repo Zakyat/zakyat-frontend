7650dc217cbabc9019cc0d89dfb14a6bbeefc231
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-08-29 16:48:21 +0300

    Move common content to root page

--- pages/help-request/index.vue
+++ pages/help-request/index.vue
@@ -1,34 +1,14 @@
 <template>
-  <v-container>
-    <h1 class="mt-9 mb-6">
-      Получить помощь
-    </h1>
-    <div class="content">
-      <!--add !currentContent for disabled components,
-      check HelpRequestCategories.vue -> data -> categories.componentName-->
-      <h2 v-if="currentContent === 'linkOfCategories' || !currentContent" class="mb-7">
-        Выберите свою категорию для получения помощи:
-      </h2>
-      <!--shows for current category-->
-      <h2 v-else class="mb-7">
-        Ваша категория для получения помощи:
-      </h2>
-      <HelpRequestCategories v-if="currentContent === 'linkOfCategories' || !currentContent" @set-content="setContent" />
-      <ChildrenWithDisabilitiesHelp v-if="currentContent === 'childrenWithDisabilities'" @set-content="setContent" />
-      <!--add here other category-components-->
-    </div>
-  </v-container>
+  <HelpRequestCategories @set-content="setContent" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelpRequestCategories from '@/components/help-requests/HelpRequestCategories.vue';
-import ChildrenWithDisabilitiesHelp from '@/components/help-requests/ChildrenWithDisabilitiesHelp.vue';
 
 export default Vue.extend({
   name: 'HelpRequestContent',
   components: {
-    ChildrenWithDisabilitiesHelp,
     HelpRequestCategories,
   },
   data () {
@@ -43,12 +23,3 @@ export default Vue.extend({
   },
 });
 </script>
-
-<style scoped lang="scss">
-  .content {
-    background-color: white;
-    border-radius: 15px;
-    padding: 40px;
-    margin-bottom: 100px;
-  }
-</style>
