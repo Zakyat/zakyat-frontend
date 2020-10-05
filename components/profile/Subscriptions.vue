f62db69b57c9dd53bf80f6c612946669adc58b66
Ural <ural2001@gmail.com>
2020-10-05 15:43:12 +0300

    Fix subscriptions cards design
    Make them with v-card instead of v-simple-table

--- components/profile/Subscriptions.vue
+++ components/profile/Subscriptions.vue
@@ -2,38 +2,45 @@
   <div class="mt-5">
     <h2>Ваши подписки</h2>
 
-    <v-simple-table>
-      <template>
-        <thead>
-          <tr>
-            <th>Сумма</th>
-            <th>Период</th>
-            <th>Назначение платежа</th>
-            <th>Способ платежа</th>
-            <th> </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>10<b>$</b></td>
-            <td>Ежедневно</td>
-            <td>
-              Фаттахова Камиля
-              <br>
-              <p class="sbor">
-                сбор №23
-              </p>
-            </td>
-            <td>Master card *2324</td>
-            <td>
-              <nuxt-link to="#" class="change-avatar">
-                Отменить подписку
-              </nuxt-link>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <v-row
+      align="center"
+      class="pl-5"
+    >
+      <v-col><p>Сумма</p></v-col>
+      <v-col><p>Период</p></v-col>
+      <v-col><p>Назначение платежа</p></v-col>
+      <v-col><p>Способ платежа</p></v-col>
+      <v-col></v-col>
+    </v-row>
+    <v-card
+      class="subscription pr-5 py-0"
+      flat
+      rounded
+      outlined
+    >
+      <div class="green"></div>
+      <v-row
+        align="center"
+        class="pl-5"
+      >
+<!--        <v-col cols="auto" class="py-0"></v-col>-->
+        <v-col>10<b>$</b></v-col>
+        <v-col>Ежедневно</v-col>
+        <v-col class="">
+          Фаттахова Камиля
+          <br>
+          <p class="sbor">
+            сбор №23
+          </p>
+        </v-col>
+        <v-col>Master card *2324</v-col>
+        <v-col>
+          <nuxt-link to="#" class="change-avatar">
+            Отменить подписку
+          </nuxt-link>
+        </v-col>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
@@ -58,5 +65,23 @@ h2 {
   padding: 0;
   margin: 0;
   color: #7f828b;
+}
+
+.subscription {
+  border-radius: 10px !important;
+  display: flex;
+}
+
+.green {
+  background-color: #1c6739;
+  width: 5px;
+  border-radius: 10px 0 0 10px !important;
+}
+
+p {
+  padding: 0;
+  margin: 0;
+  color: #7f828b;
+  font-size: 12px;
 }
 </style>
