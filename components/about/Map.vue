cec595b52b5db090ba4ace7207fefdc699b89170
Ural <ural2001@gmail.com>
2020-10-08 10:06:42 +0300

    Change map appearance

--- components/about/Map.vue
+++ components/about/Map.vue
@@ -1,5 +1,5 @@
 <template>
   <v-container class="pa-0 mt-5">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2244.035229590512!2d49.116030851759845!3d55.775259980463964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415ead01bb6604a9%3A0x6d26b8330c131671!2z0YPQuy4g0JPQsNCx0LTRg9C70LvRiyDQotGD0LrQsNGPLCA4OSwg0JrQsNC30LDQvdGMLCDQoNC10YHQvy4g0KLQsNGC0LDRgNGB0YLQsNC9LCA0MjAwMjE!5e0!3m2!1sru!2sru!4v1596530227372!5m2!1sru!2sru" width="100%" height="450" style="border: 0;" />
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8976.1410364398!2d49.11031935174631!3d55.77525946777109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415ead01bb6604a9%3A0x6d26b8330c131671!2z0YPQuy4g0JPQsNCx0LTRg9C70LvRiyDQotGD0LrQsNGPLCA4OSwg0JrQsNC30LDQvdGMLCDQoNC10YHQvy4g0KLQsNGC0LDRgNGB0YLQsNC9LCA0MjAwMjE!5e0!3m2!1sru!2sru!4v1602121655878!5m2!1sru!2sru" width="100%" height="450" style="border: 0;"></iframe>
   </v-container>
 </template>
