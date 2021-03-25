7de996f0cf7db04fd7658ef60185aa46b5722695
Ural <ural2001@gmail.com>
2021-03-25 14:27:16 +0300

    Add payment success and error pages

---
+++ pages/payment/fail.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout white class="payment-layout">
+    <v-container>
+      <div class="payment-success">
+        <v-img width="325px" :src="require('@/assets/images/payment/payment-error.png')" alt="" />
+        <div class="text-center">
+          <h1>Ошибка</h1>
+          <p>
+            Произошла ошибка при оплате
+          </p>
+        </div>
+      </div>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'PaymentFail',
+};
+</script>
+
+<style scoped>
+.payment-layout {
+  height: 600px;
+  display: flex;
+  align-items: center;
+}
+
+.payment-success {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/payment/success.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-layout white class="payment-layout">
+    <v-container>
+      <div class="payment-success">
+        <v-img width="325px" :src="require('@/assets/images/payment/payment-success.png')" alt="" />
+        <div class="text-center">
+          <h1>Успешная оплата</h1>
+          <p>
+            Спасибо! Ваше пожертвование <b>10₽</b> успешно поступило на&nbsp;счет <b>сбора &#8470;&nbsp;309.</b> <br> Смотрите историю совершенных пожертвований в&nbsp;разделе
+            <nuxt-link
+              to="/"
+              class="green-text"
+            >
+              &laquo;Мои пожертвования&raquo;.
+            </nuxt-link>
+          </p>
+        </div>
+      </div>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'PaymentSuccess',
+};
+</script>
+
+<style scoped>
+.payment-layout {
+  height: 600px;
+  display: flex;
+  align-items: center;
+}
+
+.payment-success {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.green-text {
+  color: #00ac00;
+}
+</style>
