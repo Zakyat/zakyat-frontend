6880d857acc1b418155a1da0a277bfd38936e705
Ural <ural2001@gmail.com>
2021-04-02 02:15:28 +0300

    Add volunteers dialog

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,64 +1,97 @@
 <template>
-  <v-footer absolute app color="#202020" style="display: block;">
-    <v-row
-      justify="center"
-    >
-      <v-col
-        v-for="(page, i) in pages"
-        :key="i"
-        class="text-center white--text"
-        cols="auto"
+  <div>
+    <v-footer absolute app color="#202020" style="display: block;">
+      <v-row
+        justify="center"
       >
-        <v-btn
-          :to="page.link"
-          text
-          nuxt
-          small
-          class="pa-4"
-          active-class="active"
+        <v-col
+          v-for="(page, i) in pages"
+          :key="i"
+          class="text-center white--text"
+          cols="auto"
         >
-          {{ page.text }}
-        </v-btn>
-      </v-col>
-      <v-col cols="4" sm="2" lg="1" class="py-2 px-0">
-        <LanguageSelector />
-      </v-col>
-    </v-row>
-    <v-row class="my-3">
-      <v-col class="py-2 text-center white--text" cols="12">
-        <v-btn text small>
-          ©{{ new Date().getFullYear() }}  {{ $t('copyright') }}
-        </v-btn>
-        <v-btn text small href="tel:+78432253322">
-          +7 (843) 225-33-22
-        </v-btn>
-        <v-btn text small href="mailto:fondzakyat@yandex.ru">
-          fondzakyat@yandex.ru
-        </v-btn>
-        <v-btn text small>
-          <v-icon medium>
-            mdi-vk
+          <v-btn
+            :to="page.link"
+            text
+            nuxt
+            small
+            class="pa-4"
+            active-class="active"
+          >
+            {{ page.text }}
+          </v-btn>
+        </v-col>
+        <v-col
+          class="text-center white--text"
+          cols="auto"
+        >
+          <v-btn
+            text
+            nuxt
+            small
+            class="pa-4"
+            active-class="active"
+            @click="VolunteerBanner()"
+          >
+            {{ $t('links.volunteers') }}
+          </v-btn>
+        </v-col>
+        <v-col cols="4" sm="2" lg="1" class="py-2 px-0">
+          <LanguageSelector />
+        </v-col>
+      </v-row>
+      <v-row class="my-3">
+        <v-col class="py-2 text-center white--text" cols="12">
+          <v-btn text small>
+            ©{{ new Date().getFullYear() }}  {{ $t('copyright') }}
+          </v-btn>
+          <v-btn text small href="tel:+78432253322">
+            +7 (843) 225-33-22
+          </v-btn>
+          <v-btn text small href="mailto:fondzakyat@yandex.ru">
+            fondzakyat@yandex.ru
+          </v-btn>
+          <v-btn text small>
+            <v-icon medium>
+              mdi-vk
+            </v-icon>
+            {{ $t('VK') }}
+          </v-btn>
+          <v-btn text small>
+            <v-icon medium>
+              mdi-instagram
+            </v-icon>
+            {{ $t('instagram') }}
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-footer>
+    <v-dialog
+      v-model="volunteerDialog"
+      width="600"
+      :fullscreen="$vuetify.breakpoint.mobile"
+    >
+      <div class="text-right white">
+        <v-btn icon class="mt-3 mr-3" @click="VolunteerBanner()">
+          <v-icon color="black" large>
+            mdi-close
           </v-icon>
-          {{ $t('VK') }}
         </v-btn>
-        <v-btn text small>
-          <v-icon medium>
-            mdi-instagram
-          </v-icon>
-          {{ $t('instagram') }}
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-footer>
+      </div>
+      <BecomeVolunteer />
+    </v-dialog>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import LanguageSelector from '@/components/LanguageSelector.vue';
+import BecomeVolunteer from '@/components/volunteer/BecomeVolunteer.vue';
 
 export default Vue.extend({
   components: {
     LanguageSelector,
+    BecomeVolunteer,
   },
   data () {
     return {
@@ -68,9 +101,14 @@ export default Vue.extend({
         { text: this.$t('links.reports'), link: '/reports' },
         { text: this.$t('links.news'), link: '/news' },
         { text: this.$t('links.about'), link: '/about' },
-        { text: this.$t('links.volunteers'), link: '/' },
       ],
+      volunteerDialog: false,
     };
+  },
+  methods: {
+    VolunteerBanner () {
+      this.volunteerDialog = !this.volunteerDialog;
+    },
   },
 });
 </script>
---
+++ components/volunteer/Volunteers.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="volunteers-container">
+    <h1 class="text-center">Волонтеры сайта</h1>
+
+    <div class="volunteers"></div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Volunteers',
+};
+</script>
+
+<style>
+.volunteers-container {
+  padding: 0 67px 67px 67px;
+  background-color: #fff;
+}
+
+.phone {
+  display: flex;
+  align-items: center;
+}
+</style>
