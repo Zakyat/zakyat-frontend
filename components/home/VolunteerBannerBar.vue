fe980f1f90ca39950602b2449baa820f4ffccadf
Ural <ural2001@gmail.com>
2021-04-02 02:05:16 +0300

    Make become volunteer dialog

--- components/home/VolunteerBannerBar.vue
+++ components/home/VolunteerBannerBar.vue
@@ -1,27 +1,58 @@
 <template>
-  <v-layout class="volunteer-container" row>
-    <v-flex xs12 md7>
-      <h1>{{ $t('home.volunteer.title') }}</h1>
-      <p class="my-5" style="width: 88%;">
-        {{ $t('home.volunteer.description') }}
-      </p>
-      <v-btn height="40" rounded depressed>
-        {{ $t('home.volunteer.join') }}
-      </v-btn>
-    </v-flex>
-    <v-flex xs12 md5>
-      <v-img
-        class="float-right mb-n7"
-        max-width="400px"
-        :src="require('@/assets/images/banners/womans.svg')"
-      />
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-layout class="volunteer-container" row>
+      <v-flex xs12 md7>
+        <h1>{{ $t('home.volunteer.title') }}</h1>
+        <p class="my-5" style="width: 88%;">
+          {{ $t('home.volunteer.description') }}
+        </p>
+        <v-btn height="40" @click="VolunteerBanner()" rounded depressed>
+          {{ $t('home.volunteer.join') }}
+        </v-btn>
+      </v-flex>
+      <v-flex xs12 md5>
+        <v-img
+          class="float-right mb-n7"
+          max-width="400px"
+          :src="require('@/assets/images/banners/womans.svg')"
+        />
+      </v-flex>
+    </v-layout>
+    <v-dialog
+      v-model="volunteerDialog"
+      width="600"
+      :fullscreen="$vuetify.breakpoint.mobile"
+    >
+      <div class="text-right white">
+        <v-btn icon class="mt-3 mr-3" @click="VolunteerBanner()">
+          <v-icon color="black" large>
+            mdi-close
+          </v-icon>
+        </v-btn>
+      </div>
+      <BecomeVolunteer />
+    </v-dialog>
+  </div>
 </template>
-<script lang="ts">
+
+<script>
 import Vue from 'vue';
+import BecomeVolunteer from '@/components/volunteer/BecomeVolunteer';
 
 export default Vue.extend({
+  components: {
+    BecomeVolunteer,
+  },
+  data () {
+    return {
+      volunteerDialog: false,
+    };
+  },
+  methods: {
+    VolunteerBanner () {
+      this.volunteerDialog = !this.volunteerDialog;
+    },
+  },
 });
 </script>
 
---
+++ components/volunteer/BecomeVolunteer.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="become-volunteer">
+    <h1 class="text-center">Станьте волонтером</h1>
+
+    <p class="mt-10">Если хотите стать волонтером свяжитесь с нами <br> по номеру телефона</p>
+    <div class="phone">
+      <img width="20" :src="require('@/assets/images/projects/svg/icons/phone.svg')" alt="">
+      <p class="ml-5 mb-0">
+        8-987-225-33-22.
+      </p>
+    </div>
+    <p class="mt-5">или на почту</p>
+    <div class="phone">
+      <img width="30" :src="require('@/assets/images/mail.svg')" alt="">
+      <p class="ml-5 mb-0">
+        fondzakyat@yandex.ru
+      </p>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'BecomeVolunteer',
+};
+</script>
+
+<style>
+.become-volunteer {
+  padding: 0 67px 67px 67px;
+  background-color: #fff;
+}
+
+.phone {
+  display: flex;
+  align-items: center;
+}
+</style>
