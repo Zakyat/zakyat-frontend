95c068abe84ee595af34406b6cff3f57652188cd
Ural <ural2001@gmail.com>
2021-04-25 02:22:48 +0300

    fix instagram

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -12,19 +12,19 @@
 <!--      {{ $t('search.buttonName') }}-->
 <!--    </v-btn>-->
 
-    <v-btn text smal :href="siteSettings.vk" style="text-transform: none">
+    <v-btn text smal :href="siteSettings.vk" style="text-transform: none;">
       <v-icon color="white" size="20">
         mdi-vk
       </v-icon>
 <!--      {{ $t('VK') }}-->
       Вконтакте
     </v-btn>
-    <v-btn text small :href="siteSettings.insta" style="text-transform: none; font-size: 14px">
+    <v-btn text small :href="siteSettings.insta" style="text-transform: none; font-size: 14px;">
       <v-icon color="white" size="21">
         mdi-instagram
       </v-icon>
 <!--      {{ $t('instagram') }}-->
-      Инстаграмм
+      Инстаграм
     </v-btn>
 
 <!--    Authentication-->
