4358bbb941493ed6205e934f3f252d07355ef94a
Ural <ural2001@gmail.com>
2021-04-26 00:16:48 +0300

    fix phone and email in volunteer modal

--- components/volunteer/BecomeVolunteer.vue
+++ components/volunteer/BecomeVolunteer.vue
@@ -2,26 +2,41 @@
   <v-container class="become-volunteer">
     <h1 class="text-center">Станьте волонтером</h1>
 
-    <p class="mt-10">Если хотите стать волонтером свяжитесь с нами <br> по номеру телефона</p>
+    <p class="mt-10">Если хотите стать волонтером свяжитесь с нами <br> по номеру телефона:</p>
     <div class="phone">
       <img width="20" :src="require('@/assets/images/projects/svg/icons/phone.svg')" alt="">
       <p class="ml-5 mb-0">
-        8-987-225-33-22.
+        {{ siteSettings.phone }}
       </p>
     </div>
-    <p class="mt-5">или на почту</p>
+    <p class="mt-5">или напишите на почту:</p>
     <div class="phone">
       <img width="30" :src="require('@/assets/images/mail.svg')" alt="">
       <p class="ml-5 mb-0">
-        fondzakyat@yandex.ru
+        {{ siteSettings.email }}
       </p>
     </div>
   </v-container>
 </template>
 
 <script>
+import gql from 'graphql-tag';
+
 export default {
   name: 'BecomeVolunteer',
+  apollo: {
+    siteSettings: gql`query{
+      siteSettings {
+        phone,
+        email,
+      }
+    }`,
+  },
+  data () {
+    return {
+      siteSettings: '',
+    };
+  },
 };
 </script>
 
