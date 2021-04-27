eb05499b26409f445233115435f6d19cf686e392
Ural <ural2001@gmail.com>
2021-04-27 22:19:01 +0300

    fix header background color

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div flat color="white" height="120" style="padding: 10px 5%; display: flex; flex-wrap: wrap; align-items: center;">
+  <div flat color="white" height="120" style="padding: 10px 5%; display: flex; flex-wrap: wrap; align-items: center; background-color: white;">
     <nuxt-link to="/">
       <img :src="require(`@/assets/logo/${$i18n.locale}.svg`)">
     </nuxt-link>
@@ -70,6 +70,7 @@ export default Vue.extend({
 .v-btn--active::before {
   opacity: 0;
 }
+
 .v-btn {
   text-transform: none;
 }
