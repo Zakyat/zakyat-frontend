09583d2601fc4f2dd398b781fea32d333e7b5830
Ural <ural2001@gmail.com>
2021-07-01 00:15:17 +0300

    Add email confirmation page

---
+++ pages/email-confirmation.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout white class="payment-layout">
+    <v-container>
+      <div class="payment-success">
+        <v-img width="250px" :src="require('@/assets/images/payment/payment-success.png')" alt="successImg" />
+        <div class="text-center">
+          <h1>Спасибо!</h1>
+          <p class="my-0">
+            Ваша почта <b>"email"</b> успешно подтверждена!
+          </p>
+        </div>
+      </div>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "email-confirmation"
+    }
+</script>
+
+<style scoped>
+  .payment-layout {
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .payment-success {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+
